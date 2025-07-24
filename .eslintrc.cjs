fee0a87068dcbd6c7d35bd9ff15dac6e4a76f36e
chy031202 <chy031202@gmail.com>
2025-07-24 20:34:50 +0900

    [fix] eslint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,6 @@ module.exports = {
         // 세미콜론이 없으면 에러로 취급한다.
         semi: ['off'],
         // 기존 프로젝트에서는 'warn'으로 취급되지만, 'error'로 설정하면 에러로 취급한다.
-        '@typescript-eslint/no-unused-vars': 'error'
+        '@typescript-eslint/no-unused-vars': 'warn'
     }
 };
