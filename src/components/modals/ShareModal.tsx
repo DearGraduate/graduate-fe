ea0114df1abb3a61d29e5002042c0cf9c7e67bb9
chy031202 <chy031202@gmail.com>
2025-08-10 16:59:54 +0900

    [feat] 카카오로 공유하기 기능

--- src/components/modals/ShareModal.tsx
+++ src/components/modals/ShareModal.tsx
@@ -5,6 +5,7 @@ import Xcross from "../../assets/icons/Xcross.png";
 import KaKao from "../../assets/icons/KAKAOMini.png";
 import Link from "../../assets/icons/Link.png";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 // interface LoginModalProps extends ModalProps {
 
@@ -12,6 +13,7 @@ import { useLocation } from "react-router-dom";
 
 const ShareModal = ({ isOpen, onRequestClose }: ModalProps) => {
 
+    const [step, setStep] = useState<'default' | 'kakao'>('default');
     const location = useLocation();
     console.log(location);
 
@@ -20,46 +22,73 @@ const ShareModal = ({ isOpen, onRequestClose }: ModalProps) => {
 
     console.log(`${baseURL}${pathUrl}`);
 
-    const clipUrl = async (url : string) => {
+    const clipUrl = async (url: string) => {
         try {
             await navigator.clipboard.writeText(url);
             alert("링크가 복사되었습니다!");
-        } catch(err) {
+        } catch (err) {
             alert("링크 복사에 실패했습니다. 다시 시도해주세요.");
         }
     }
 
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            contentLabel="공유하기 모달"
-            className="flex justify-center "
-            style={customModalStyles}
-            >
-            <button className="absolute top-[11px] right-[20px] w-[24px] h-[24px]" onClick={onRequestClose}>
-                <img src={Xcross} alt="나가기 버튼" />
-            </button>
-            <div className="flex flex-col justify-center items-center pt-6 pb-9 w-[87%] text-center">
-                <h3 
-                style={{ fontFamily: 'Ydestreet', fontWeight: '700' }}
-                className="text-lg font-semibold mb-3 w-full whitespace-nowrap text-center">공유하기</h3>
+    const TEMPLATE_ID = process.env.REACT_KAKAO_TEMPLATE_ID; 
 
-                <div 
-                style={{ fontFamily: 'Ydestreet', fontWeight: '300' }}
-                className="flex flex-row justify-center gap-[10px]">
-                    <button 
-                    className="px-[15px] py-[15px] flex flex-col items-center gap-3 whitespace-nowrap text-center text-gray-800 active:bg-gray-100"
+    const ShareKaKao = () => {
+        window.Kakao.Share.sendCustom({
+            templateId: TEMPLATE_ID,
+        //     templateArgs: {
+        //         title: '제목 영역입니다.',
+        //         description: '설명 영역입니다.',
+        // },
+        });
+    }
+
+return (
+    <Modal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        contentLabel="공유하기 모달"
+        className="flex justify-center "
+        style={customModalStyles}
+    >
+        <button className="absolute top-[11px] right-[20px] w-[24px] h-[24px]" onClick={onRequestClose}>
+            <img src={Xcross} alt="나가기 버튼" />
+        </button>
+        <div className="flex flex-col justify-center items-center pt-6 pb-9 w-[87%] text-center">
+            <h3
+                style={{ fontFamily: 'Ydestreet', fontWeight: '700' }}
+                className="text-lg font-semibold mb-3 w-full whitespace-nowrap text-center">
+                {step === 'default' ? ('공유하기')
+                    : step === 'kakao' ? '카카오톡 공유하기'
+                        : ''}</h3>
+
+
+            {step === 'default' && (
+                <div
+                    style={{ fontFamily: 'Ydestreet', fontWeight: '300' }}
+                    className="flex flex-row justify-center gap-[10px]">
+                    <button
+                        onClick={
+                            () => ShareKaKao()
+                        }
+                        className="px-[15px] py-[15px] flex flex-col items-center gap-3 whitespace-nowrap text-center text-gray-800 active:bg-gray-100"
                     ><img src={KaKao} height={'26px'} width={'30px'} alt="카카오" />카카오톡</button>
 
                     <button
-                    onClick={() => clipUrl(`${baseURL}${pathUrl}`)}
-                    className="px-[15px] py-[15px] flex flex-col items-center gap-3 whitespace-nowrap text-center text-gray-800  active:bg-gray-100"
+                        onClick={() => clipUrl(`${baseURL}${pathUrl}`)}
+                        className="px-[15px] py-[15px] flex flex-col items-center gap-3 whitespace-nowrap text-center text-gray-800  active:bg-gray-100"
                     ><img src={Link} height={'26px'} width={'26px'} alt="링크복사" />링크복사</button>
                 </div>
-            </div>
-        </Modal>
-    )
+            )}
+
+            {step === 'kakao' && (
+                <></>
+            )}
+
+
+        </div>
+    </Modal>
+)
 }
 
 export default ShareModal;
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,29 @@ import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
+// 카카오 JavaScript SDK 초기화
+const JAVASCRIPT_KEY =  process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY;
+
+if (window.Kakao && !window.Kakao.isInitialized()) {
+  if (!JAVASCRIPT_KEY) {
+    console.error('Kakao JavaScript Key is missing.');
+  } else {
+    window.Kakao.init(JAVASCRIPT_KEY);
+  }
+}
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>
