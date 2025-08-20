d3dec7d2363eab5594a0e6e278915479c0f20e3c
chy031202 <chy031202@gmail.com>
2025-08-20 22:35:01 +0900

    [feat] 모달 부착(수정필요)

--- src/components/common/MessageCard.tsx
+++ src/components/common/MessageCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import testImage from '../../assets/icons/img_default.png'
 import icondots from '../../assets/icons/icon_dots.png'
+import EditDeleteBottomSheet from '../modals/EditDeleteModal'
+import {useState } from 'react';
 
 export interface MessageProps {
   name: string
@@ -10,16 +12,26 @@ export interface MessageProps {
 }
 
 const MessageCard: React.FC<MessageProps> = ({ name, imageUrl, message, detailClick }) => {  const src = imageUrl && imageUrl.trim() !== '' ? imageUrl : (testImage as unknown as string)
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+        setModalOpen(true);     
+    }
+        const closeModal = () => {
+        setModalOpen(false);
+    }   
 
   return (
     <div className="bg-letterBox rounded-2xl p-4 shadow-md w-full max-w-[200px] text-center">
       {/* 상단 이름 + 더보기 아이콘 */}
       <div className="flex justify-between items-center text-black font-semibold text-[14px] mb-1 font-ydestreet">
         <span className="truncate">{name}</span>
-        <button type="button" onClick={detailClick} aria-label="더보기" className="p-1">
+        <button type="button" onClick={openModal} aria-label="더보기" className="p-1">
           <img src={icondots} alt="더보기" className="w-4 h-4 object-contain" />
         </button>
       </div>
+      
+      <EditDeleteBottomSheet isOpen={modalOpen} onRequestClose={closeModal}/>
 
       <img
         src={src}
--- src/components/modals/EditDeleteModal.tsx
+++ src/components/modals/EditDeleteModal.tsx
@@ -4,8 +4,14 @@ import type { ModalProps } from "../../types/Modal";
 import CustomButton from "../common/button";
 export const SHEET_SNAP_POINTS = [0.7, 0.5, 0.3];
 export const SHEET_INITIAL_SNAP = 2;
+import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 const EditDeleteBottomSheet = ({ isOpen, onRequestClose }: ModalProps) => {
+    const isWindow = useMediaQuery({ query: "(min-width: 1025px)" });
+    const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+    const isTablet = useMediaQuery({ query: "(min-width: 768px) and (max-width: 1024px), (max-height:1024px)" });
+    const navigate = useNavigate();
     const ref = useRef<SheetRef>(null);
     const [, setSnapPoint] = useState<number>(SHEET_INITIAL_SNAP);
 
@@ -17,22 +23,29 @@ const EditDeleteBottomSheet = ({ isOpen, onRequestClose }: ModalProps) => {
             onSnap={setSnapPoint}
             snapPoints={SHEET_SNAP_POINTS}
             initialSnap={SHEET_INITIAL_SNAP}
-            
+
         >
             <Sheet.Backdrop onTap={onRequestClose} />
             <Sheet.Container
-                className="bg-white rounded-t-[40px] h-[15vh] w-full"
+                className={`bg-white rounded-t-[40px]
+    ${isWindow
+                        ? 'max-w-[360px] mx-auto'
+                        : isTablet
+                            ? 'w-screen'
+                            : isMobile
+                                ? 'w-screen' : ''}`}
             >
                 <Sheet.Header />
                 <Sheet.Content>
                     <div className="flex flex-col justify-center items-center p-10 w-full text-center mb-5">
                         <h3
-                            style={{ fontFamily: 'Ydestreet', fontWeight: '700', fontSize:'14px' }}
+                            style={{ fontFamily: 'Ydestreet', fontWeight: '700', fontSize: '14px' }}
                             className="text-lg font-semibold mb-6 w-full whitespace-nowrap text-center text-black">
                             축하글을 수정하거나 삭제하시겠어요?</h3>
 
                         <div className="flex flex-row justify-center gap-[12px]">
                             <CustomButton
+                                onClick={() => navigate('/writing')}
                                 className="px-[15px] py-[15px] w-fit whitespace-nowrap text-center"
                             >축하글 수정하기</CustomButton>
                             <CustomButton
