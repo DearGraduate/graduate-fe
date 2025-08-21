2064516e47be2d2c0525cfa6225996cc54d8da43
chy031202 <chy031202@gmail.com>
2025-08-21 21:00:51 +0900

    [fix] 공유하기 base url 변경

--- src/components/modals/ShareModal.tsx
+++ src/components/modals/ShareModal.tsx
@@ -34,61 +34,64 @@ const ShareModal = ({ isOpen, onRequestClose }: ModalProps) => {
     const TEMPLATE_ID = Number(process.env.REACT_APP_KAKAO_TEMPLATE_ID?.trim());
 
     const ShareKaKao = () => {
+        const shareUrl = `${baseURL}${pathUrl}`;
         window.Kakao.Share.sendCustom({
             templateId: TEMPLATE_ID,
-        //     templateArgs: {
-        //         title: '제목 영역입니다.',
-        //         description: '설명 영역입니다.',
-        // },
+            templateArgs: {
+                // 템플릿에서 쓴 변수명과 동일해야 함
+                url: shareUrl,
+                // 필요시 제목/설명 등도 변수로 만들어 전달 가능
+                // title: '제목', description: '설명'
+            },
         });
     }
 
-return (
-    <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-        contentLabel="공유하기 모달"
-        className="flex justify-center "
-        style={customModalStyles}
-    >
-        <button className="absolute top-[11px] right-[20px] w-[24px] h-[24px]" onClick={onRequestClose}>
-            <img src={Xcross} alt="나가기 버튼" />
-        </button>
-        <div className="flex flex-col justify-center items-center pt-6 pb-9 w-[87%] text-center">
-            <h3
-                style={{ fontFamily: 'Ydestreet', fontWeight: '700' }}
-                className="text-lg font-semibold mb-3 w-full whitespace-nowrap text-center">
-                {step === 'default' ? ('공유하기')
-                    : step === 'kakao' ? '카카오톡 공유하기'
-                        : ''}</h3>
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            contentLabel="공유하기 모달"
+            className="flex justify-center "
+            style={customModalStyles}
+        >
+            <button className="absolute top-[11px] right-[20px] w-[24px] h-[24px]" onClick={onRequestClose}>
+                <img src={Xcross} alt="나가기 버튼" />
+            </button>
+            <div className="flex flex-col justify-center items-center pt-6 pb-9 w-[87%] text-center">
+                <h3
+                    style={{ fontFamily: 'Ydestreet', fontWeight: '700' }}
+                    className="text-lg font-semibold mb-3 w-full whitespace-nowrap text-center">
+                    {step === 'default' ? ('공유하기')
+                        : step === 'kakao' ? '카카오톡 공유하기'
+                            : ''}</h3>
 
 
-            {step === 'default' && (
-                <div
-                    style={{ fontFamily: 'Ydestreet', fontWeight: '300' }}
-                    className="flex flex-row justify-center gap-[10px]">
-                    <button
-                        onClick={
-                            () => ShareKaKao()
-                        }
-                        className="px-[15px] py-[15px] flex flex-col items-center gap-3 whitespace-nowrap text-center text-gray-800 active:bg-gray-100"
-                    ><img src={KaKao} height={'26px'} width={'30px'} alt="카카오" />카카오톡</button>
+                {step === 'default' && (
+                    <div
+                        style={{ fontFamily: 'Ydestreet', fontWeight: '300' }}
+                        className="flex flex-row justify-center gap-[10px]">
+                        <button
+                            onClick={
+                                () => ShareKaKao()
+                            }
+                            className="px-[15px] py-[15px] flex flex-col items-center gap-3 whitespace-nowrap text-center text-gray-800 active:bg-gray-100"
+                        ><img src={KaKao} height={'26px'} width={'30px'} alt="카카오" />카카오톡</button>
 
-                    <button
-                        onClick={() => clipUrl(`${baseURL}${pathUrl}`)}
-                        className="px-[15px] py-[15px] flex flex-col items-center gap-3 whitespace-nowrap text-center text-gray-800  active:bg-gray-100"
-                    ><img src={Link} height={'26px'} width={'26px'} alt="링크복사" />링크복사</button>
-                </div>
-            )}
+                        <button
+                            onClick={() => clipUrl(`${baseURL}${pathUrl}`)}
+                            className="px-[15px] py-[15px] flex flex-col items-center gap-3 whitespace-nowrap text-center text-gray-800  active:bg-gray-100"
+                        ><img src={Link} height={'26px'} width={'26px'} alt="링크복사" />링크복사</button>
+                    </div>
+                )}
 
-            {step === 'kakao' && (
-                <></>
-            )}
+                {step === 'kakao' && (
+                    <></>
+                )}
 
 
-        </div>
-    </Modal>
-)
+            </div>
+        </Modal>
+    )
 }
 
 export default ShareModal;
